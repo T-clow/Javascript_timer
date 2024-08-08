97e8a7fb2f8bbd6d7503b65083bb89c600d541fb
T-clow <813.danger@gmail.com>
2024-08-08 16:16:45 +0900

    javascriptを作成

---
+++ timer.js
@@ -0,0 +1,34 @@
+let finish
+let intervalId
+let startButton = document.querySelector("#start-button")
+startButton.addEventListener("click", startTimer)
+let stopButton = document.querySelector("#stop-button")
+stopButton.addEventListener("click", stopTimer)
+
+function startTimer() {
+  let second = document.querySelector("#time-input").value
+  finish = Date.now() + second * 1000
+  intervalId = setInterval(checkRemainingTime, 50)
+  startButton.disabled = true
+}
+
+function stopTimer() {
+  clearInterval(intervalId)
+  setDisplay(0)
+  startButton.disabled = false
+}
+
+function checkRemainingTime() {
+  let remain = finish - Date.now()
+  let second = Math.floor(remain / 1000) + 1
+  setDisplay(second)
+  if (remain <= 0) {
+    stopTimer()
+    alert("時間になりました")
+  }
+}
+
+function setDisplay(second) {
+  let countDown = document.querySelector("#count-down")
+  countDown.textContent = second
+}
